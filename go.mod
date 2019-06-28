0cfc01e570ee31d7423da5e492fc3509756b49f2
Daz Wilkin <dazwilkin@google.com>
2019-06-28 12:49:26 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/DazWilkin/basic-personality
+
+go 1.12
+
+require (
+	github.com/gogo/protobuf v1.2.1
+	github.com/google/trillian v1.2.1
+	github.com/grpc-ecosystem/grpc-gateway v1.9.2 // indirect
+	google.golang.org/grpc v1.21.1
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/google/trillian"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	serviceName = "image-transparency-server"
+)
+
+var (
+	tLogEndpoint = flag.String("tlog_endpoint", "", "The gRPC endpoint of the Trillian Log Server.")
+	tLogID       = flag.Int64("tlog_id", 0, "Trillian Log ID")
+)
+
+func main() {
+	flag.Parse()
+
+	// Establish gRPC connection w/ Trillian Log Server
+	log.Printf("[main] Establishing connection w/ Trillian Log Server [%s]", *tLogEndpoint)
+	conn, err := grpc.Dial(*tLogEndpoint, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// Create a Trillian Log Server client
+	log.Println("[main] Creating new Trillian Log Client")
+	tLogClient := trillian.NewTrillianLogClient(conn)
+
+	// Eventually this personality will be a server
+	log.Printf("[main] Creating Server using LogID [%d]", *tLogID)
+	server := newServer(tLogClient, *tLogID)
+
+	// Leaves comprise a primary LeafValue (thing) and may have associated ExtraData(extra)
+	// The LeafValue will become the hashed value for a node in the Merkle Tree
+	log.Println("[main] Creating a 'Thing' and something 'Extra'")
+	thing := newThing(fmt.Sprintf("[%s] Thing", time.Now().Format(time.RFC3339)))
+	extra := newExtra("Extra")
+
+	// Eventually it will be convenient to explicit Request and Response types
+	resp := &Response{}
+
+	// Try to put this Request (Thing+Extra) in the Log
+	log.Println("[main] Submitting it for inclusion in the Trillian Log")
+	resp, err = server.put(&Request{
+		thing: *thing,
+		extra: *extra,
+	})
+	log.Printf("[main] put: %s", resp.status)
+
+	// Try to get this Request (Thing+Extra) from the Log
+	log.Println("[main] Retrieving it from the Trillian Log")
+	resp, err = server.get(&Request{
+		thing: *thing,
+		extra: *extra,
+	})
+	log.Printf("[main] get: %s", resp.status)
+}
